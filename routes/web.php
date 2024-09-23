e107f0849667c595ab204bb0e66e3acc0c7ed396
Elvis Eneque <ayrtoneneque@gmail.com>
2024-09-23 13:57:25 -0500

    up

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', [ProductoController::class, 'index']);
 
 
